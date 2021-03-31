202cfc3aa94c63289ae0aa8aec4966e0015d8665
Rizky-Muslimin <riskimusliminanxel12@gmail.com>
2021-03-31 18:53:48 +0700

    latihan1

---
+++ app/src/main/java/com/example/biodata_akb_if1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.biodata_akb_if1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
